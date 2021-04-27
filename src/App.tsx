611d5b7af31508a643fe72ed4e48c623ca35a49a
dwyte <dwyte@tutanota.com>
2021-04-27 22:19:32 +0800

    Added basic visual player controls.

--- src/App.tsx
+++ src/App.tsx
@@ -10,36 +10,78 @@ const Container = styled.div`
   height: 100%;
 `;
 
+const generateRandomGraphData = () =>
+  Array(50)
+    .fill(0)
+    .map((_, index) => ({
+      id: index,
+      value: Math.round(Math.max(Math.random(), 0.01) * 100),
+    }));
+
 const App = () => {
-  const [graphData, setGraphData] = useState<Bar[]>(
-    Array(100)
-      .fill(0)
-      .map((_, index) => ({
-        id: index,
-        value: Math.round(Math.max(Math.random(), 0.01) * 100),
-      }))
-  );
+  const [graphData, setGraphData] = useState<Bar[]>(generateRandomGraphData());
 
   const [graphDataSteps, setGraphDataSteps] = useState<Bar[][]>([graphData]);
   const [graphDataStep, setGraphDataStep] = useState<number>(0);
 
+  const [playTimeout, setPlayTimeout] = useState<NodeJS.Timeout | null>(null);
+
   useEffect(() => {
     setGraphDataSteps(generateSoringSteps(graphData));
+    setGraphDataStep(0);
   }, [graphData]);
 
-  const playSorting = () => {
+  const handlePrevious = () => {
     setGraphDataStep((currentGraphDataStep) => {
-      if (currentGraphDataStep < graphDataSteps.length - 1) {
-        return currentGraphDataStep + 1;
+      if (currentGraphDataStep > 0) {
+        return currentGraphDataStep - 1;
       }
 
       return currentGraphDataStep;
     });
   };
 
+  const handleRandom = () => {
+    setGraphData(generateRandomGraphData());
+  };
+
+  const handleNext = () => {
+    setGraphDataStep((currentGraphDataStep) => {
+      if (currentGraphDataStep < graphDataSteps.length - 1) {
+        return currentGraphDataStep + 1;
+      }
+      return currentGraphDataStep;
+    });
+  };
+
+  const handlePlay = () => {
+    if (!playTimeout) {
+      setPlayTimeout(setInterval(handleNext, 10));
+    }
+  };
+
+  const handlePause = () => {
+    if (playTimeout) {
+      clearInterval(playTimeout);
+      setPlayTimeout(null);
+    }
+  };
+
   return (
     <Container>
-      <button onClick={() => setInterval(playSorting, 100)}>Start</button>
+      <button onClick={handleRandom} disabled={Boolean(playTimeout)}>
+        Random
+      </button>
+      <button onClick={handlePrevious} disabled={Boolean(playTimeout)}>
+        {"<"}
+      </button>
+      {playTimeout && <button onClick={handlePause}>Pause</button>}
+      {!playTimeout && <button onClick={handlePlay}>Play</button>}
+      <button onClick={handleNext} disabled={Boolean(playTimeout)}>
+        {">"}
+      </button>
+      <button disabled>{graphDataStep}</button>
+
       <Graph data={graphDataSteps[graphDataStep]} />
     </Container>
   );
