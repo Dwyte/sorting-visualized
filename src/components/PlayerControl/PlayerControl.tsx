f927da378b8a84495df2038ed7924d637103016d
dwyte <dwyte@tutanota.com>
2021-05-23 19:05:34 +0800

    Added StepButton styling.

--- src/components/PlayerControl/PlayerControl.tsx
+++ src/components/PlayerControl/PlayerControl.tsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 
 import { IsPlayingContext } from "../../contexts/IsPlayingContext";
-import { StepProgress, Container } from "./styles";
+import { StepProgress, Container, StepButton } from "./styles";
 
 interface Props {
   currentStep: number;
@@ -26,14 +26,25 @@ export const PlayerControl: React.FC<Props> = ({
 
   return (
     <Container>
-      <button onClick={onPrevious} disabled={isPlaying}>
-        {"<"}
-      </button>
-      {isPlaying && <button onClick={onPause}>Pause</button>}
-      {!isPlaying && <button onClick={onPlay}>Play</button>}
-      <button onClick={onNext} disabled={isPlaying}>
-        {">"}
-      </button>
+      <StepButton onClick={onPrevious} disabled={isPlaying}>
+        <i className="fas fa-step-backward"></i>
+      </StepButton>
+
+      {isPlaying && (
+        <StepButton onClick={onPause}>
+          <i className="fas fa-pause"></i>
+        </StepButton>
+      )}
+
+      {!isPlaying && (
+        <StepButton onClick={onPlay}>
+          <i className="fas fa-play"></i>
+        </StepButton>
+      )}
+
+      <StepButton onClick={onNext} disabled={isPlaying}>
+        <i className="fas fa-step-forward"></i>
+      </StepButton>
 
       <StepProgress
         type="range"
--- src/components/PlayerControl/styles.ts
+++ src/components/PlayerControl/styles.ts
@@ -2,6 +2,20 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
+  gap: 4px;
+`;
+
+export const StepButton = styled.button`
+  background-color: #222;
+  padding: 8px 24px;
+  border-radius: 0;
+  cursor: pointer;
+  border: none;
+  color: white;
+
+  :hover {
+    background-color: #333;
+  }
 `;
 
 export const StepProgress = styled.input`
@@ -17,15 +31,15 @@ export const StepProgress = styled.input`
     -moz-appearance: none;
     border-radius: 0;
     border: none;
-    height: 24px;
-    width: 14px;
+    height: 32px;
+    width: 20px;
   }
 
   // for chrome and safari
   ::-webkit-slider-thumb {
     -webkit-appearance: none;
     background: white;
-    height: 24px;
-    width: 14px;
+    height: 32px;
+    width: 20px;
   }
 `;
