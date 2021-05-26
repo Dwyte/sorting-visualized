1fbfeceb52a21c525f085d7886ab02448061a2a3
dwyte <dwyte@tutanota.com>
2021-05-26 20:51:14 +0800

    Fixed gap on graph.

--- src/components/Graph/styles.ts
+++ src/components/Graph/styles.ts
@@ -4,12 +4,12 @@ export const Container = styled.div`
   height: 100%;
   display: flex;
   align-items: flex-end;
+  gap: 2px;
 `;
 
 export const BarItem = styled.div<{ value: number; color?: string }>`
   background-color: ${({ color }) => color || "white"};
   height: ${({ value }) => value}%;
-  margin: 0 1px;
   flex: 1;
 
   cursor: pointer;
